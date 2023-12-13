0b124fec608d215c80cf536cc3a165b7b7ba5b6a
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2023-12-13 12:38:15 +0000

    sign in check on following page

--- app/following/page.tsx
+++ app/following/page.tsx
@@ -3,9 +3,16 @@ import { buttonVariants } from "@/components/ui/button";
 import { getFollowedUsers, getPostsByFollowedUsers } from "@/lib/data";
 import { auth } from "@/auth";
 import Post from "@/components/post/Post";
+import AuthCheck from "@/components/AuthCheck";
+import { redirect } from "next/navigation";
 
 const Following = async () => {
   const session = await auth();
+
+  if (!session?.user) {
+    redirect("/api/auth/signin?callbackUrl=/following");
+  }
+
   const id = session?.user.id!;
   const followedUsers = await getFollowedUsers(id);
   const followedUserIds = followedUsers?.following.map(
@@ -16,23 +23,26 @@ const Following = async () => {
   return (
     <PageWrapper>
       <h1>Posts by users you follow:</h1>
-      {postsByFollowedUsers.length ? (
-        postsByFollowedUsers.map((post) => {
-          return (
-            <Post
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              content={post.content!}
-              author={post?.author!}
-              publishedAt={post?.publishedAt}
-              authorId={post?.authorId!}
-            />
-          );
-        })
-      ) : (
-        <p>Nothing to see here!</p>
-      )}
+      <AuthCheck>
+        {postsByFollowedUsers.length ? (
+          postsByFollowedUsers.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                content={post.content!}
+                author={post?.author!}
+                publishedAt={post?.publishedAt}
+                authorId={post?.authorId!}
+              />
+            );
+          })
+        ) : (
+          <p>Nothing to see here!</p>
+        )}
+      </AuthCheck>
+      
     </PageWrapper>
   );
 };
